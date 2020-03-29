3514414a1e1dbe521dbc7bd1023a1542d39ea405
Lario <osunkunleolanrewaju@gmail.com>
2020-03-29 16:39:34 +0100

    contact data from created and linked to firestore

--- Desktop/lario/projects/react/burger-builder/src/components/Input/Input.js
+++ Desktop/lario/projects/react/burger-builder/src/components/Input/Input.js
@@ -5,17 +5,36 @@ import styles from './Input.module.css'
 const Input = (props) => {
     let inputElement = null
 
-    switch (props.inputtype) {
+    switch (props.elementType) {
         case ('input'):
-            inputElement = <input className={styles.InputElement} {...props} />
+            inputElement = <input className={styles.InputElement}
+                            {...props.elementConfig}
+                            value={props.value}
+                             onChange={props.changed} />
             break;
 
         case ('textarea'):
-            inputElement = <textarea {...props} />
+            inputElement = <textarea className={styles.InputElement}
+                            {...props.elementConfig}
+                            value={props.value}
+                             onChange={props.changed}/>
             break;
-    
+
+        case ('select'):
+            inputElement = <select className={styles.InputElement}
+                            value={props.value}
+                            onChange={props.changed}>
+                                {props.elementConfig.options.map(option => (
+                                    <option key={option.value} value={option.value}>{option.displayValue}</option>
+                                ))}
+                            </select>
+
+            break;
+
         default:
-           inputElement = <input {...props} />
+           inputElement = <input className={styles.InputElement}
+                            {...props.elementConfig}
+                            value={props.value} />
     }
 
     return (
--- Desktop/lario/projects/react/burger-builder/src/container/Checkout/ContactData/ContactData.js
+++ Desktop/lario/projects/react/burger-builder/src/container/Checkout/ContactData/ContactData.js
@@ -9,11 +9,43 @@ import styles  from './ContactData.module.css'
 
 class ContactData extends Component {
     state = {
-        name : '',
-        email : '',
-        address : {
-            street: '',
-            postalCode: ''
+        orderForm : {
+            name : {
+                elementType : 'input',
+                elementConfig : {
+                    type : 'text',
+                    placeholder : 'Your name'
+                },
+                value : ''
+            },
+            country : {
+                elementType: 'input',
+                elementConfig : {
+                    type : 'text',
+                    placeholder : 'Country'
+                },
+                value : ''
+            },
+            email : {
+                elementType: 'input',
+                elementConfig : {
+                    type : 'email',
+                    placeholder : 'Enter email'
+                },
+                value : ''
+            },
+            deliveryMethod : {
+                elementType: 'select',
+                elementConfig : {
+                    options: [
+                        {value : 'fastest', displayValue : 'Fastest'},
+                        {value : 'cheapest', displayValue : 'Cheapest'}
+                    ]
+                },
+
+                value : ''
+            }
+            
         },
         loading: false
     }
@@ -21,15 +53,14 @@ class ContactData extends Component {
     orderHandler = (event) => {
         event.preventDefault()
         this.setState({loading: true})
+        const formData = {}
+        for (let formIdentifier in this.state.orderForm){
+            formData[formIdentifier] = this.state.orderForm[formIdentifier].value
+        }
         const order = {
-            ingredients : this.props.ingredients,
+            ingredients: this.props.ingredients,
             price : this.props.price,
-            customer : {
-                name : "Larrieman",
-                country : "Nigeria"
-            },
-            email : "test@test.com",
-            deliveryMethod : "fastest delivery"
+            orderData : formData
         }
         axios.post('https://burger-builder-cfa8a.firebaseio.com/order.json', order)
             .then(res =>{ 
@@ -41,16 +72,42 @@ class ContactData extends Component {
             .catch(error => this.setState({loading : false}))
     }
 
-    render() {
-        let form = (
-            <form>
-                    <Input inputtype="input"  type="text" name="name" placeholder="Enter your name" />
-                    <Input inputtype="input"  type="email" name="email" placeholder="Enter your email" />
-                    <Input inputtype="input"  type="text" name="street" placeholder="Enter your street" />
-                    <Input inputtype="input"  type="text" name="postal code" placeholder="Enter your postal code" />
+    inputChangeHandler = (event, inputIdentifier) => {
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        }
+        const updatedFormElement = {
+            ...updatedOrderForm[inputIdentifier]
+        }
+        updatedFormElement.value = event.target.value
+        updatedOrderForm[inputIdentifier] = updatedFormElement
+        this.setState({orderForm: updatedOrderForm})
+    }
 
-                    <Buttons btnType="Success"
-                    clicked={this.orderHandler} >ORDER NOW</Buttons>
+    render() {
+        const orderFormArray = []
+        for (let key in this.state.orderForm){
+            orderFormArray.push({
+                id: key,
+                config : this.state.orderForm[key]
+            })
+        }
+
+        let form = (
+            <form onSubmit={this.orderHandler}>
+                    {
+                        orderFormArray.map(formElement => (
+                            <Input
+                                key={formElement.id}
+                                elementType={formElement.config.elementType}
+                                elementConfig = {formElement.config.elementConfig}
+                                value={formElement.config.value}
+                                changed={(event) => this.inputChangeHandler(event,  formElement.id)}
+                            />
+                        ))
+                    }
+
+                    <Buttons btnType="Success" >ORDER NOW</Buttons>
             </form>
         )
         if (this.state.loading){
